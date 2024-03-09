bd586da57304ebcec40e7634bc1f1e4ac4d9447c
Neit <vitkovskijnikita43@gmail.com>
2024-03-09 09:45:32 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+number = 100
+
+name = "name "
+
+surname = "surname"
+
+b = True
+
+f = 54.34
+
+import random
+
+l = [35, True, "test", 35.353, name, random.randint(1, 1000)]
+
+print(l)
+
+l.pop(2)
+
+print(l)
+
+l.remove(True)
+
+print(l)
+
+
+
+print(name + surname)
